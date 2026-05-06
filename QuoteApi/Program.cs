474db499f99af7035daf44e5f7a2194da2160413
Nigel Thompson <nigel.thompson@sociustec.com>
2026-05-06 15:54:41 +0100

    debug

--- QuoteApi/Program.cs
+++ QuoteApi/Program.cs
@@ -2,12 +2,17 @@ using QuoteApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("ServiceBus")
+    ?? builder.Configuration["ServiceBus:ConnectionString"]
+    ?? builder.Configuration["ServiceBusConnectionString"];
+
+Console.WriteLine($"DEBUG: ConnectionString found: {!string.IsNullOrEmpty(connectionString)}");
+Console.WriteLine($"DEBUG: ConnectionStrings__ServiceBus: {builder.Configuration.GetConnectionString("ServiceBus") ?? "(null)"}");
+Console.WriteLine($"DEBUG: ServiceBus__ConnectionString: {builder.Configuration["ServiceBus:ConnectionString"] ?? "(null)"}");
+Console.WriteLine($"DEBUG: ServiceBusConnectionString: {builder.Configuration["ServiceBusConnectionString"] ?? "(null)"}");
+
 builder.Services.AddSingleton(_ =>
-    new ServiceBusPublisher(
-        builder.Configuration.GetConnectionString("ServiceBus")
-        ?? builder.Configuration["ServiceBus:ConnectionString"]
-        ?? builder.Configuration["ServiceBusConnectionString"]!
-    ));
+    new ServiceBusPublisher(connectionString!));
 
 var app = builder.Build();
 
